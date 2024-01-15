3aa86a46d21b61934f404a1f0375ed4ec5cf5d4a
Amine <asbaay@iesebre.com>
2024-01-15 19:18:54 +0100

    Laravelapp 1.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Task::factory(5)->create();
+        \App\Models\Task::factory()->create([
+         'name' => 'Test User',
+         'description' => 'Descripcio Sbaay',
+         'completed' => true,
+        ]);
+
+        \App\Models\User::factory(5)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Guillem',
+            'email' => 'gsancho@iesebre.com',
+            'password' => 'guillem1589',
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
 
@@ -25,4 +27,11 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
+}); */
+
+Route::get('/',[UserController::class,'index']);
+Route::get('/users',[UserController::class,'index']);
+Route::get('/contact',[PagesController::class,'contact']);
+Route::get('/about',[PagesController::class,'about']);
+Route::get('/tasks',[\App\Http\Controllers\TaskController::class,'index']);
+
